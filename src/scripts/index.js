6b5bc88669575803169b3885e6592945de11d6e7
Pratikshavsanjekar <59285726+Pratikshavsanjekar@users.noreply.github.com>
2020-05-08 18:42:24 +0530

    updated js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,13 +2,13 @@ let savedData= null;
 
 
 let url= "https://newsapi.org/v2/top-headlines?country=in&apiKey=d84c79b5c841468d829306c08c9ebd4e";
-// let url ="https://newsapi.org/v2/everything?q=bitcoin&apiKey=d84c79b5c841468d829306c08c9ebd4e";
+ 
 let search = function(keyword){
  let searchUrl = url + `&q=${keyword}`;
  callApi(searchUrl);
 }
 let ShowAll= function(){
-    document.getElementById("results").style.display = "grid";
+    document.getElementById("news-articles").style.display = "grid";
     document.getElementById("SingleResults").style.display = "none";
 
  
@@ -19,13 +19,13 @@ let handleClick =function(elementNum){
     let htmlChunk ="template data";
     document.getElementById("SingleResults").innerHTML = htmlChunk;
     
-    document.getElementById("results").style.display = "none";
+    document.getElementById("news-articles").style.display = "none";
 
 
 }
 let handleSearch =function(){
     if(event.keyCode==13){
-        var searchInput = document.getElementById("SearchBox");
+        var searchInput = document.getElementById("search");
         search(searchInput.value);
     }
     
@@ -35,27 +35,33 @@ let prepareHTMLFromData = function (dataArr){
 let finalHTML ='';
 if(dataArr.length == 0){
     document.getElementById("ResultCount").innerHTML="";
-    document.getElementById("results").innerHTML = "no results";
+    //document.getElementById("news-articles").innerHTML = "no results";
+    document.getElementById("news-articles").innerHTML = '<li class="not-found">No article was found based on the search.</li>';
     return;
 }
+
 
 for(let i = 0; i < dataArr.length; i++){
     console.log(dataArr[i]);
 
     let htmlString = `
-    <div class="item" onclick="handleClick(${i})">
-    <img class="imgStyle" src="${dataArr[i]["urlToImage"]}"> 
-        <div>${dataArr[i]["title"]}</div> 
-       <div>${dataArr[i]["description"]} </div> 
-       <div>${dataArr[i]["author"]} </div> 
-       </div>` ;
+    
+    <li class="article" onclick="handleClick(${i})">
+    <div class="img_area">
+    <img Class="article-img" src="${dataArr[i]["urlToImage"]}"> </div>
+        <h2  class="article-title">${dataArr[i]["title"]}</h2> 
+        <p class="article-description">${dataArr[i]["description"]} </p><br>
+        <span class="article-author">${dataArr[i]["author"]} </span> <br>
+        
+       </li>` ;
 
         finalHTML = finalHTML + htmlString;
 console.log(finalHTML);
 
+
 }
 document.getElementById("ResultCount").innerHTML= `Result Count ${dataArr.length}`;
-document.getElementById("results").innerHTML = finalHTML;
+document.getElementById("news-articles").innerHTML = finalHTML;
 // document.getElementById("results").innerHTML = htmlString;
 }
 
